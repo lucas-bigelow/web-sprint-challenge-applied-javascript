acee121a9fc6cad28afedf742d1b01586dc571c1
Lucas Bigelow <lukejbigelow@gmail.com>
2022-02-11 12:00:57 -0600

    completed tasks 5 and 6 in the card component

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,6 +19,41 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+
+  // step 1 is to create all the elements
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const authorPhoto = document.createElement("img");
+  const imgCredits = document.createElement("span");
+
+  // step 2 is to add the nonspecific data (and event listener in this case)
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  card.addEventListener("click", evt => {
+    evt.preventDefault();
+    console.log(article.headline);
+  })
+
+  // step 3 is to add the data dependent on the argument
+  headline.textContent = article.headline;
+  authorPhoto.src = article.authorPhoto;
+  imgCredits.textContent = `By: ${article.authorName}`;
+
+  // step 4 is to stitch it all together (in order of inheritance) and return card
+  imgContainer.appendChild(authorPhoto);
+
+  author.appendChild(imgContainer);
+  author.appendChild(imgCredits);
+
+  card.appendChild(headline);
+  card.appendChild(author);
+
+  return card;
 }
 
 const cardAppender = (selector) => {
@@ -28,6 +65,19 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+
+  axios.get("http://localhost:5000/api/articles")
+    .then(resp => resp.data)
+    .then(resp => {
+      // loop over the articles after the data has been parsed (articles is an object, so looping requires turning it into an array)
+      Object.keys(resp.articles).forEach(article => {
+        // now loop over the author data in each article, create a card, and append it
+        resp.articles[article].forEach(author => { 
+          document.querySelector(selector).appendChild(Card(author));
+        })
+      })
+    })
+    .catch(err => console.error(err));
 }
 
 export { Card, cardAppender }
