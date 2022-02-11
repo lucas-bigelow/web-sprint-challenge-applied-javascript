dbf5a49d94b01469030288b6356792fe8d1d831c
Lucas Bigelow <lukejbigelow@gmail.com>
2022-02-11 11:20:02 -0600

    completed tasks 1 and 2 inside the header component

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,29 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  // create html elements
+  const header = document.createElement("div");
+  const dateSpan = document.createElement("span");
+  const titleH1 = document.createElement("h1");
+  const tempSpan = document.createElement("span");
+
+  // add the non-unique info
+  header.classList.add("header");
+  dateSpan.classList.add("date");
+  tempSpan.classList.add("temp");
+
+  // add unique info using arguments
+  dateSpan.textContent = date;
+  titleH1.textContent = title;
+  tempSpan.textContent = temp;
+
+  //  now we put it all together and return it
+  header.appendChild(dateSpan);
+  header.appendChild(titleH1);
+  header.appendChild(tempSpan);
+
+  return header;
 }
 
 const headerAppender = (selector) => {
@@ -20,6 +43,8 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+
+  document.querySelector(selector).appendChild(Header("Foo", "1999", "Bar"));
 }
 
 export { Header, headerAppender }
